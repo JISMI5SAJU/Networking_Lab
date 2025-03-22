f60dc369a2ccd255ecf800e3677bac66024f537e
JISMI5SAJU <146074614+JISMI5SAJU@users.noreply.github.com>
2025-03-22 21:24:11 +0530

    Update dvr.c

--- dvr2.c
+++ dvr2.c
@@ -1,43 +1,46 @@
 #include<stdio.h>
-struct router {
-    unsigned distance[20];
-    unsigned nextHop[20];
-} routingTable[10];
+struct node {
+    unsigned dist[20];
+    unsigned from[20];
+} rt[10];
 
 int main() {
-    int costMatrix[20][20];
-    int totalNodes, i, j, k, updateCount = 0;
-    printf("\nEnter the number of nodes: ");
-    scanf("%d", &totalNodes);
-    printf("\nEnter the cost matrix:\n");
+    int costmat[20][20];
+    int nodes, i, j, k, count = 0;
     
-    for(i = 0; i < totalNodes; i++) {
-        for(j = 0; j < totalNodes; j++) {
-            scanf("%d", &costMatrix[i][j]);
-            costMatrix[i][i] = 0;
-            routingTable[i].distance[j] = costMatrix[i][j];
-            routingTable[i].nextHop[j] = j; 
+    printf("\nEnter the number of nodes : ");
+    scanf("%d", &nodes);
+    
+    printf("\nEnter the cost matrix :\n");
+    for(i = 0; i < nodes; i++) {
+        for(j = 0; j < nodes; j++) {
+            scanf("%d", &costmat[i][j]);
+            costmat[i][i] = 0;
+            rt[i].dist[j] = costmat[i][j];
+            rt[i].from[j] = j;
         }
     }
     
     do {
-        updateCount = 0;
-        for(i = 0; i < totalNodes; i++) 
-            for(j = 0; j < totalNodes; j++)
-                for(k = 0; k < totalNodes; k++)
-                    if(routingTable[i].distance[j] > costMatrix[i][k] + routingTable[k].distance[j]) {
-                        routingTable[i].distance[j] = routingTable[i].distance[k] + routingTable[k].distance[j];
-                        routingTable[i].nextHop[j] = k;
-                        updateCount++;
+        count = 0;
+        for(i = 0; i < nodes; i++) {
+            for(j = 0; j < nodes; j++) {
+                for(k = 0; k < nodes; k++) {
+                    if(rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) {
+                        rt[i].dist[j] = costmat[i][k] + rt[k].dist[j];
+                        rt[i].from[j] = k;
+                        count++;
                     }
-    } while(updateCount != 0);
-
-    for(i = 0; i < totalNodes; i++) {
+                }
+            }
+        }
+    } while(count != 0);
+    
+    for(i = 0; i < nodes; i++) {
         printf("\n\n For router %d\n", i + 1);
-        for(j = 0; j < totalNodes; j++) {
-            printf("\t\nnode %d via %d Distance %d ", j + 1, routingTable[i].nextHop[j] + 1, routingTable[i].distance[j]);
+        for(j = 0; j < nodes; j++) {
+            printf("\t\nnode %d via %d Distance %d ", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
         }
     }
     printf("\n\n");
 }
-
